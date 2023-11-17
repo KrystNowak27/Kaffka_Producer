77e2fdaf1ed5ceece80bb67de335d387a59ee04f
Krystian <krystian.nowak27@gmail.com>
2023-11-17 14:41:34 +0100

    Producer Kafka end RestTemplate

--- src/main/java/org/nowak/controller/RestController.java
+++ src/main/java/org/nowak/controller/RestController.java
@@ -9,14 +9,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
 @Profile("prod")
-@RestController
+@org.springframework.web.bind.annotation.RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
-public class ClientController {
+public class RestController {
 
     private final RestClient restClient;
 
